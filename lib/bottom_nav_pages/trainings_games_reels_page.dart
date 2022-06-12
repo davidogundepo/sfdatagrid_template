7840c2fce146592f53d1971af1515296bb669f23
David Ogundepo <david.oludepo@gmail.com>
2022-06-12 07:53:46 +0100

    Updating

--- lib/bottom_nav_pages/trainings_games_reels_page.dart
+++ lib/bottom_nav_pages/trainings_games_reels_page.dart
@@ -1,7 +1,104 @@
 
 import 'package:flutter/material.dart';
 
-// class TrainingsAndGamesReelsPage extends StatelessWidget implements PreferredSizeWidget {
+import '../main.dart';
+
+class TrainingsAndGamesReelsPage extends StatelessWidget implements PreferredSizeWidget {
+
+  final Function onPressed;
+  final Function onTitleTapped;
+
+  @override
+  final Size preferredSize;
+
+  const TrainingsAndGamesReelsPage(
+      {Key key, @required this.onPressed, this.onTitleTapped})
+      : preferredSize = const Size.fromHeight(60.0),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          // SizedBox(height: 30,),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Hero(
+                tag: 'trainingsAndGamesReelsPageBtn',
+                child: Card(
+                  elevation: 10,
+                  shape: kBackButtonShape,
+                  // child: MaterialButton(
+                  //   height: 50,
+                  //   minWidth: 50,
+                  //   elevation: 10,
+                  //   shape: kBackButtonShape,
+                  //   onPressed: onPressed,
+                  //   // child: child,
+                  // ),
+                  child: IconButton(
+                    icon: kBackBtn, onPressed: () {
+                    Navigator.of(context).pop(false);
+                    // Navigator.of(context);
+                    navigateMyApp(context);
+                  },
+
+                  ),
+                ),
+              ),
+              // SizedBox(
+              //   width: 50,
+              // ),
+              Hero(
+                tag: 'title',
+                transitionOnUserGestures: true,
+                child: Card(
+                  elevation: 10,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(30),
+                    ),
+                  ),
+                  child: InkWell(
+                    onTap: onTitleTapped,
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width / 1.5,
+                      height: 50,
+                      child: const Align(
+                        alignment: Alignment.center,
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 30),
+                          child: Text(
+                            'Monthly Reels',//ADD FOOTBALL ICON
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                              // color: Colors.black54,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future navigateMyApp(context) async {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => const MyApp()));
+  }
+
+}
+
+// class TrainingsAndGamesReelsPage extends StatefulWidget implements PreferredSizeWidget {
 //
 //   final Function onPressed;
 //   final Function onTitleTapped;
@@ -9,11 +106,16 @@ import 'package:flutter/material.dart';
 //   @override
 //   final Size preferredSize;
 //
-//   const TrainingsAndGamesReelsPage(
-//       {Key key, @required this.onPressed, this.onTitleTapped})
+//   const TrainingsAndGamesReelsPage({Key key, @required this.onPressed, this.onTitleTapped})
 //       : preferredSize = const Size.fromHeight(60.0),
 //         super(key: key);
 //
+//   @override
+//   State<TrainingsAndGamesReelsPage> createState() => _TrainingsAndGamesReelsPageState();
+//
+// }
+//
+// class _TrainingsAndGamesReelsPageState extends State<TrainingsAndGamesReelsPage> {
 //   @override
 //   Widget build(BuildContext context) {
 //     return SafeArea(
@@ -34,7 +136,8 @@ import 'package:flutter/material.dart';
 //                     minWidth: 50,
 //                     elevation: 10,
 //                     shape: kBackButtonShape,
-//                     onPressed: onPressed,
+//                     onPressed: () {  },
+//                     // onPressed: onPressed,
 //                     // child: child,
 //                   ),
 //                 ),
@@ -53,7 +156,7 @@ import 'package:flutter/material.dart';
 //                     ),
 //                   ),
 //                   child: InkWell(
-//                     onTap: onTitleTapped,
+//                     // onTap: onTitleTapped,
 //                     child: SizedBox(
 //                       width: MediaQuery.of(context).size.width / 1.5,
 //                       height: 50,
@@ -77,13 +180,13 @@ import 'package:flutter/material.dart';
 //               )
 //             ],
 //           ),
+//
+//           Container(),
 //         ],
 //       ),
 //     );
 //   }
-//
 // }
-
 
 ShapeBorder kBackButtonShape = const RoundedRectangleBorder(
   borderRadius: BorderRadius.only(
@@ -95,91 +198,3 @@ Widget kBackBtn = const Icon(
   Icons.arrow_back_ios,
   color: Colors.black54,
 );
-
-class TrainingsAndGamesReelsPage extends StatefulWidget implements PreferredSizeWidget {
-
-
-  final Function onPressed;
-  final Function onTitleTapped;
-
-  @override
-  final Size preferredSize;
-
-  const TrainingsAndGamesReelsPage({Key key, @required this.onPressed, this.onTitleTapped})
-      : preferredSize = const Size.fromHeight(60.0),
-        super(key: key);
-
-  @override
-  State<TrainingsAndGamesReelsPage> createState() => _TrainingsAndGamesReelsPageState();
-}
-
-class _TrainingsAndGamesReelsPageState extends State<TrainingsAndGamesReelsPage> {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: <Widget>[
-          // SizedBox(height: 30,),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Hero(
-                tag: 'trainingsAndGamesReelsPageBtn',
-                child: Card(
-                  elevation: 10,
-                  shape: kBackButtonShape,
-                  child: MaterialButton(
-                    height: 50,
-                    minWidth: 50,
-                    elevation: 10,
-                    shape: kBackButtonShape,
-                    onPressed: () {  },
-                    // onPressed: onPressed,
-                    // child: child,
-                  ),
-                ),
-              ),
-              // SizedBox(
-              //   width: 50,
-              // ),
-              Hero(
-                tag: 'title',
-                transitionOnUserGestures: true,
-                child: Card(
-                  elevation: 10,
-                  shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(30),
-                    ),
-                  ),
-                  child: InkWell(
-                    // onTap: onTitleTapped,
-                    child: SizedBox(
-                      width: MediaQuery.of(context).size.width / 1.5,
-                      height: 50,
-                      child: const Align(
-                        alignment: Alignment.centerLeft,
-                        child: Padding(
-                          padding: EdgeInsets.only(left: 30),
-                          child: Text(
-                            'Monthly Reels',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 25,
-                              // color: Colors.black54,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-}
