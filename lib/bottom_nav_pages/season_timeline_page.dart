d1d6485eb300d5b83386b953366f25f9ba82acf2
David Ogundepo <david.oludepo@gmail.com>
2022-06-21 14:14:42 +0100

    rewers

--- lib/bottom_nav_pages/season_timeline_page.dart
+++ lib/bottom_nav_pages/season_timeline_page.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
 import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
 import 'package:avatar_glow/avatar_glow.dart';
 
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:provider/provider.dart';
+import '../notifier/player_of_the_month_stats_info_notifier.dart';
+
 class SeasonTimeline extends StatefulWidget {
   const SeasonTimeline({Key key}) : super(key: key);
 
@@ -15,6 +19,10 @@ class _SeasonTimelineState extends State<SeasonTimeline> {
 
   @override
   Widget build(BuildContext context) {
+
+    PlayerOfTheMonthStatsAndInfoNotifier playerOfTheMonthStatsAndInfoNotifier = Provider.of<PlayerOfTheMonthStatsAndInfoNotifier>(context);
+
+
     const backgroundColor = Color(0xFFE7ECEF);
     Offset distance = isPressed ? const Offset(2, -2) : const Offset(2, -2);
     double blur = isPressed ? 7.0 : 24.0;
@@ -27,101 +35,128 @@ class _SeasonTimelineState extends State<SeasonTimeline> {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.only(left: 40.0, right: 40, top: 120, bottom: 120),
-          child: Stack(
-            children: [
+          child:
+          ListView.builder(
+            itemExtent: MediaQuery.of(context).size.height * 0.585,
+            shrinkWrap: true,
+            itemCount: playerOfTheMonthStatsAndInfoNotifier.playerOfTheMonthStatsAndInfoList.length,
+            itemBuilder: (context, index) =>
+                Stack(
+                    alignment: AlignmentDirectional.topStart,
+                    // fit: StackFit.loose,
+                  children: [
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * .43,
+                        height: MediaQuery.of(context).size.height * 2,
 
-              Align(
-                alignment: Alignment.centerLeft,
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * .43,
-                  height: MediaQuery.of(context).size.height,
-                  child: Image.asset(
-                      "assets/images/curry_champion.jpeg",
-                    fit: BoxFit.cover,
-                    color: Colors.teal,
-                    colorBlendMode: BlendMode.softLight,
-                    alignment: const Alignment(0.4, -1),
-                  ),
-                ),
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: SizedBox(
-                  // color: Colors.green,
-                  height: MediaQuery.of(context).size.height,
-                  // height: 500,
-                  width: MediaQuery.of(context).size.width,
-                  child: Image.asset(
-                    "assets/images/back_field.png",
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              ),
-              Align(
-                alignment: Alignment.topRight,
-                child: Container(
+                        // decoration: BoxDecoration(
+                        //   image: DecorationImage(
+                        //     alignment: const Alignment(0, -1),
+                        //     image: Image(image: CachedNetworkImageProvider(
+                        //       playerOfTheMonthStatsAndInfoNotifier.playerOfTheMonthStatsAndInfoList[index].image,
+                        //     )),
+                        //     fit: BoxFit.cover,
+                        //   ),
+                          // shape: BoxShape.rectangle,
+                        // ),
+                        child: Image(
 
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Column(
-                        children: [
-                          Text(
-                            'sense',
-                            style: TextStyle(
-                              color: Colors.white70
-                            ),
+                          // "assets/images/curry_champion.jpeg",
+                          image: CachedNetworkImageProvider(
+                            playerOfTheMonthStatsAndInfoNotifier.playerOfTheMonthStatsAndInfoList[index].image,
                           ),
-                          Text(
-                            'sense 2 years',
-                            style: TextStyle(
-                                color: Colors.white70
-                            ),
-                          ),
-                          SizedBox(height: 30),
-                        ],
+                          fit: BoxFit.cover,
+                          color: Colors.grey,
+                          colorBlendMode: BlendMode.softLight,
+                          alignment: const Alignment(0.4, -1),
+                        ),
                       ),
-                      Column(
-                        children: [
-                          Text(
-                            'sense',
-                            style: TextStyle(
-                              color: Colors.white70
-                            ),
-                          ),
-                          Text(
-                            'sense 2 years',
-                            style: TextStyle(
-                                color: Colors.white70
-                            ),
-                          ),
-                          SizedBox(height: 30),
-                        ],
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: SizedBox(
+                        // color: Colors.green,
+                        height: MediaQuery.of(context).size.height,
+                        // height: 500,
+                        width: MediaQuery.of(context).size.width,
+                        child: Image.asset(
+                          "assets/images/back_field.png",
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                      Column(
-                        children: [
-                          Text(
-                            'sense',
-                            style: TextStyle(
-                              color: Colors.white70
+                    ),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Container(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Column(
+                              children: [
+                                Text(
+                                  'sense 1',
+                                  style: TextStyle(
+                                      color: Colors.white70,
+                                    fontSize: 30
+                                  ),
+                                ),
+                                Text(
+                                  // 'sense 1 years',
+                                  playerOfTheMonthStatsAndInfoNotifier.playerOfTheMonthStatsAndInfoList[index].playerName,
+                                  style: TextStyle(
+                                      color: Colors.white70,
+                                      fontSize: 30
+                                  ),
+                                ),
+                                SizedBox(height: 30),
+                              ],
                             ),
-                          ),
-                          Text(
-                            'sense 2 years',
-                            style: TextStyle(
-                                color: Colors.white70
+                            Column(
+                              children: [
+                                Text(
+                                  'sense 2',
+                                  style: TextStyle(
+                                      color: Colors.white70
+                                  ),
+                                ),
+                                Text(
+                                  playerOfTheMonthStatsAndInfoNotifier.playerOfTheMonthStatsAndInfoList[index].playerPosition,
+                                  style: TextStyle(
+                                      color: Colors.white70
+                                  ),
+                                ),
+                                SizedBox(height: 30),
+                              ],
                             ),
-                          ),
-                          SizedBox(height: 30),
-                        ],
+                            Column(
+                              children: [
+                                Text(
+                                  'sense 3',
+                                  style: TextStyle(
+                                      color: Colors.white70
+                                  ),
+                                ),
+                                Text(
+                                  'sense 3 years',
+
+                                  style: TextStyle(
+                                      color: Colors.white70
+                                  ),
+                                ),
+
+                                SizedBox(height: 30),
+                              ],
+                            ),
+                          ],
+                        ),
                       ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+                    ),
+                  ],
+                    ),
+          )
         ),
       ),
 
